5f857f6acadbc528f740084c26584437d1d7e7ff
Adam <adambecker3@gmail.com>
2020-09-27 22:03:37 +0100

    header component set up

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import headerData from './config/header-config.js';
 import logo from './logo.svg';
 
+const newInTagline = "New In: Pleasures | Rick Owens | Stone Island";
+
 const Header = () => {
-  return (
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    </header>
-  );
+    const [ headers, setHeaders ] = useState();
+    const [ selectedItem, setSelectedItem ] = useState();
+
+    useEffect(() => {
+        const dataHeaders = headerData.navCatagories.map(({ name, id }) => ({ name, id }));
+        setHeaders(dataHeaders);
+    }, []);
+
+    return (
+        <header className="header">
+            <div className="header__tagline">{newInTagline}</div>
+            <div className="header__logo" >
+                <img className="header__logo-image" src={logo} alt="logo" />
+            </div>
+            <div className="header__menu">
+                {headers && headers.map(({ name, id }) => {
+                    return <span 
+                        key={`${name}_${id}`}
+                        className="header__menu-item"
+                        onMouseLeave={() => setSelectedItem()}
+                        onMouseEnter={() => setSelectedItem(id)}>
+                            {name}
+                        </span>;
+                })}
+            </div>
+            {selectedItem && <div>{selectedItem}</div>}
+        </header>
+    );
 }
 
 export default Header;
---
+++ src/Menu.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import headerData from './config/header-config.js';
+
+
+const Menu = () => {
+
+}
+
+export default Menu;
